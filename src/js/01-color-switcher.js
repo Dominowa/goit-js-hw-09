39d016a30e38700c6b2c1dec13b8cfe18ea3f518
Dominowa <babicyura@gmail.com>
2023-08-14 15:40:09 +0300

    1st dz

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+let intervalId;
+
+function startColorChange() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  intervalId = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorChange() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(intervalId);
+  document.body.style.backgroundColor = '';
+}
+
+document
+  .querySelector('[data-start]')
+  .addEventListener('click', startColorChange);
+document
+  .querySelector('[data-stop]')
+  .addEventListener('click', stopColorChange);
